ebdd1589c1363538bb8758ede52cb31929430057
Han <57443614+currybeefer@users.noreply.github.com>
2022-05-19 10:52:38 -0600

    add wall and interactable object

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -6,28 +6,27 @@ public class playerScript : MonoBehaviour
 {
     Rigidbody2D body;
 
-    float horizontal;
-    float vertical;
-
     public float runSpeed = 20.0f;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        
     }
 
     private void FixedUpdate()
     {
-        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector2 dir = new Vector2(h, v);
+        dir *= runSpeed;
+
+        body.velocity = dir;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
