76ce99b16f62485722c338e69633192eabf6915b
hanxuan5 <hanxuan5@illinois.edu>
2022-05-19 10:04:30 -0500

    Update prefabs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager : MonoBehaviourPunCallbacks
 {
     public GameObject readyButton;
     public GameObject scriptScroll;
+    public GameObject playerPrefab;
+    public GameObject canvas;
 
     public void Start()
     {
@@ -18,6 +20,7 @@ public class GameManager : MonoBehaviourPunCallbacks
     public void ReadyButton()
     {
         readyButton.SetActive(false);
-        PhotonNetwork.Instantiate("Player", new Vector3(1, 1, 0), Quaternion.identity, 0);
+        Instantiate(playerPrefab, canvas.transform.position, Quaternion.identity, canvas.transform);
+        //PhotonNetwork.Instantiate("Player", new Vector3(1, 1, 0), Quaternion.identity, 0);
     }
 }
