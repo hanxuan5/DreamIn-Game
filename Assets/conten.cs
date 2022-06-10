2364ed24bdf8281a2e6345f82d6b944413275dfa
hanxuan5 <hanxuan5@illinois.edu>
2022-06-10 03:38:22 -0500

    Update

---
+++ Assets/conten.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class conten : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<TMP_Text>().text = "a\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\n";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
